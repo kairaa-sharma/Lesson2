e43a1664d93571c0171097c2db97e710015fbffd
Kairaa2014 <kairaasharma22@gmail.com>
2025-10-18 13:01:50 +0530

    demo

--- Storing/bdays.py
+++ Storing/bdays.py
@@ -1,13 +1,20 @@
 # ask for input assigning variables
-bday1 = str(input("What's friend 1's birthday? : "))
-bday2 = str(input("What's friend 2's birthday? : "))
-bday3 = str(input("What's friend 3's birthday? : "))
-bday4 = str(input("What's friend 4's birthday? : "))
-bday5 = str(input("What's friend 5's birthday? : "))
+frnd1 = input("What's friend 1's name? ")
+frnd2 = input("What's friend 2's name? ")
+frnd3 = input("What's friend 3's name? ")
+frnd4 = input("What's friend 4's name? ")
+frnd5 = input("What's friend 5's name? ")
+
+# ask for birthdays with proper formatting
+bday1 = input(f"What is {frnd1}'s birthday? : ")
+bday2 = input(f"What is {frnd2}'s birthday? : ")
+bday3 = input(f"What is {frnd3}'s birthday? : ")
+bday4 = input(f"What is {frnd4}'s birthday? : ")
+bday5 = input(f"What is {frnd5}'s birthday? : ")
 
 # print out the birthdays 
-print("Friend 1's birthday is", bday1)
-print("Friend 2's birthday is", bday2)
-print("Friend 3's birthday is", bday3)
-print("Friend 4's birthday is", bday4)
-print("Friend 5's birthday is", bday5)
+print(f"{frnd1}'s birthday is {bday1}")
+print(f"{frnd2}'s birthday is {bday2}")
+print(f"{frnd3}'s birthday is {bday3}")
+print(f"{frnd4}'s birthday is {bday4}")
+print(f"{frnd5}'s birthday is {bday5}")
