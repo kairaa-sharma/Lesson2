fe490df1d612ebc2ef891b79b959c2c87e0eeb18
Kairaa2014 <kairaasharma22@gmail.com>
2025-10-18 12:37:12 +0530

    demo

---
+++ Storing/bdays.py
@@ -0,0 +1,13 @@
+# ask for input assigning variables
+bday1 = str(input("What's friend 1's birthday? : "))
+bday2 = str(input("What's friend 2's birthday? : "))
+bday3 = str(input("What's friend 3's birthday? : "))
+bday4 = str(input("What's friend 4's birthday? : "))
+bday5 = str(input("What's friend 5's birthday? : "))
+
+# print out the birthdays 
+print("Friend 1's birthday is", bday1)
+print("Friend 2's birthday is", bday2)
+print("Friend 3's birthday is", bday3)
+print("Friend 4's birthday is", bday4)
+print("Friend 5's birthday is", bday5)
